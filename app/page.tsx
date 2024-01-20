98bbdbd1887c3099b08a26fc6ccf06d635847698
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2024-01-20 21:06:22 +0530

    chore: increase font size

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
 	return (
 		<main className="flex flex-col items-start justify-between p-28 max-md:p-8 overflow-hidden relative">
 			<Image src="/images/logo.svg" alt="Logo" height={50} width={120} className="w-auto max-md:w-16" />
-			<h1 className="text-5xl tracking-normal my-2 w-1/3 leading-tight text-primary max-md:text-4xl">
-				Google Developer Student Clubs
-			</h1>
+			<h1 className="text-5xl tracking-normal my-2 w-1/3 leading-tight text-primary">Google Developer Student Clubs</h1>
 			<p className="text-2xl py-4 text-muted-foreground max-md:text-lg max-md:py-2">RCOEM CHAPTER</p>
 			<p className="text-2xl tracking-normal my-2 w-2/4 leading-tight text-muted-foreground max-md:w-full max-md:text-sm">
 				Developer Student Clubs are university based community groups for students interested in Google developer
